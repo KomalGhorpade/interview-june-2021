94508d0ee92f1522863dc918d851646c55449d94
KomalGhorpade <86234790+KomalGhorpade@users.noreply.github.com>
2021-06-22 18:17:05 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -4,7 +4,46 @@ public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
+    int ans = 0;
+        for (int i = total - k; i <= total; i++) 
+        {
+     
+            int ways = nCr(total, i);
+    
+            ans += (ways * countDerangements(total- i));
+        }
+        return ans;
   }
-
-}
+    
+    
+    
+    
+    static int nCr(int total, int k)
+    {
+        int ans = 1;
+        if (r > total - r)
+            r = total - r;
+        for (int i = 0; i < r; i++) {
+            ans *= (n - i);
+            ans /= (i + 1);
+        }
+        return ans;
+    }
+     
+    
+    static int countDerangements(int total)
+    {
+        int der[] = new int[ total + 3];
+     
+        der[0] = 1;
+        der[1] = 0;
+        der[2] = 1;
+     
+        for (int i = 3; i <= total; i++)
+            der[i] = (i - 1) * (der[i - 1]
+                            + der[i - 2]);
+        return der[total];
+    }
+     
+    
+    }
